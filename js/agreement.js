bbaa23d0c7c22295f499f6562ecf8d24ad2cc4a9
kevinchunyu <kevin11@uw.edu>
2022-11-01 16:39:04 -0700

    passphrase checking

--- js/agreement.js
+++ js/agreement.js
@@ -17,12 +17,32 @@
         let inputButton = document.getElementById('agreement-input');
         if(inputButton.checked) {
             //send response
-            let data = await fetch('https://lgbtqspaces-api.herokuapp.com/api/send-login-information');
-            console.log(data);
+            if(! (document.getElementById('error-text').classList.contains('d-none'))) {
+                document.getElementById('error-text').classList.add('d-none');
+            }
+            let data = await getData();
+            let element = document.getElementById('data-pass');
+            let showContent = document.getElementById('show-content');
+            showContent.classList.remove('d-none');
+            element.textContent = data.password;
         } else {
+            if(! (document.getElementById('show-content').classList.contains('d-none'))) {
+                document.getElementById('show-content').classList.add('d-none');
+            }
             //display small
             let small = document.getElementById('error-text');
+            small.classList.remove('d-none');
             small.textContent = 'Please agree to our terms and conditions.';
+        }
+    }
+
+    async function getData() {
+        try {
+            let data = await fetch('https://lgbtqspaces-api.herokuapp.com/api/send-login-information', {method: 'GET'});
+            let dataJSON = await data.json();
+            return dataJSON[0];
+        } catch (err) {
+            console.log(err);
         }
     }
 })();
