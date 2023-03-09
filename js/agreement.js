bf45a8c0ea974ea5cca12c8669f4db9748b854c7
Liz <33111846+lizhip21@users.noreply.github.com>
2023-03-08 19:39:28 -0800

    Pages updates
    Added two new pages: methodology and ethics. Adjusted the nav bar and reworked the login panel and agreement page.

--- js/agreement.js
+++ js/agreement.js
@@ -7,32 +7,40 @@
     // init function
     // added init to async (test for bugs!)
     async function init() {
-        let submitButton = document.getElementById("user-response");
+        let submitButton = document.getElementById("user-submit");
         submitButton.addEventListener('click', async function(event) {
+            event.preventDefault();
             await checkResponse();
         })
     }
-    
+
     async function checkResponse() {
         let inputButton = document.getElementById('agreement-input');
-        if(inputButton.checked) {
-            //send response
+        if(!inputButton.checked) {
+            //display small
+            let small = document.getElementById('error-text');
+            small.classList.remove('d-none');
+            small.textContent = 'Please agree to our terms and conditions.';
+            return;
+        } else if (!grecaptcha.getResponse()){
+            let small = document.getElementById('error-text');
+            small.classList.remove('d-none');
+            small.textContent = 'Please complete the reCaptcha verification.';
+            return;
+        } else {
+          //send response
             if(! (document.getElementById('error-text').classList.contains('d-none'))) {
                 document.getElementById('error-text').classList.add('d-none');
             }
             let data = await getData();
             let element = document.getElementById('data-pass');
             let showContent = document.getElementById('show-content');
+            let agreementForm = document.getElementById('agreement-form');
+            let statement = document.getElementById('statement');
             showContent.classList.remove('d-none');
+            statement.classList.add('d-none');
+            agreementForm.classList.add('d-none');
             element.textContent = data.password;
-        } else {
-            if(! (document.getElementById('show-content').classList.contains('d-none'))) {
-                document.getElementById('show-content').classList.add('d-none');
-            }
-            //display small
-            let small = document.getElementById('error-text');
-            small.classList.remove('d-none');
-            small.textContent = 'Please agree to our terms and conditions.';
         }
     }
 
