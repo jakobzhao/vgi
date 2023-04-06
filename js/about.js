0887cc0fca16443e21aea99ea60cdfd19b948bba
Liz <33111846+lizhip21@users.noreply.github.com>
2023-04-06 04:31:26 -0700

    various page updates

--- js/about.js
+++ js/about.js
@@ -1,17 +1,13 @@
-window.onload = function collapse() {
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-  console.log(document.getElementsByClassName("collapsible"))
-  console.log(coll.length)
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("collapsed");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight){
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  }
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("collapsed");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
